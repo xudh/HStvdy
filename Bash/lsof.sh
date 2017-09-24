726e32d25db3176f44c68d12d4d1f0230ba38580
xudh <xudh@xudh.me>
2017-09-24 20:04:39 +0800

    modify： lsof.sh

--- Bash/lsof.sh
+++ Bash/lsof.sh
@@ -4,7 +4,7 @@
 
 if [ $# -ne 1 ]
 then
-	echo "usage: $0 file"
+	echo "usage: $0 file/directory"
 	exit -1
 fi
 
@@ -16,10 +16,26 @@ else
 	filePath=`pwd`/$1	
 fi
 
+if [ ! -e $filePath ]
+then
+	echo "$filePath not exist"
+	exit -1
+fi
+
+fileType=0			# 0/1/2分别表示文件/文件夹/文件夹下任何文件
+if [[ $filePath == */ ]]
+then
+	fileType=2
+elif [ -d $filePath ]
+then
+	fileType=1
+fi
+
 selfPid=$$
 allpids=`ps -e -opid`
 allpids=${allpids:6}	# 斩断开头的"  PID " 
 
+matched=0
 for pid in $allpids
 do
 	if [ $pid -eq $selfPid ]	# 排除自身
@@ -29,18 +45,38 @@ do
 
 	if [ -d /proc/$pid/fd ]
 	then
+		bMatch=0
 		for file in /proc/$pid/fd/*
 		do
 			if [ -L $file ]
 			then
 				lsret=`ls -l $file`
-				if [[ $lsret == *$filePath ]]
+				if [ $fileType -eq 2 ]	# 文件夹下的任意文件
 				then
-					ps -p $pid -opid,cmd
-					break
+					if [[ $lsret == *$filePath* ]]
+					then
+						bMatch=1
+						break
+					fi
+				else	# 文件或文件夹本身
+					if [[ $lsret == *$filePath ]]
+					then
+						bMatch=1
+						break
+					fi
 				fi
 			fi
 		done
+		if [ $bMatch -eq 1 ]
+		then
+			if [ $matched -eq 0 ]
+			then
+				ps -p $pid -opid,cmd
+			else
+				ps -p $pid -opid,cmd | tail -n 1
+			fi
+			matched=1
+		fi
 	fi
 done
 
